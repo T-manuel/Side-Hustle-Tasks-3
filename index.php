36be65a6b575db37a576c36a5567ac348579ec5a
Emmanuel <26921479+T-manuel@users.noreply.github.com>
2021-03-22 16:19:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -26,22 +26,22 @@ if (isset ($_POST["register"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link href ="login.css" type = "text/css" rel = "stylesheet" >
+    <link href ="styles/login.css?v=<?php echo time();?>" type = "text/css" rel = "stylesheet" >
 </head>
 <body>
     <div class="container">
         <form class = "first-form" action = "index.php" method = "POST" autocomplete="off">
-                        <div class="form-group-2">
+            <div class="form-group-2">
                 <label for = "usernames"> Username </label>
-                <input name = "usernames" value = <?php echo $user_a; ?> class = "formControl" type = "text" required>
+                <input name = "usernames" placeholder = "8 - 14 characters" class = "formControl" type = "text" required>
             </div>
             <div class="form-group-2">
                 <label for = "surname"> Surname </label>
-                <input name = "surname" value = <?php echo $user_b; ?> class = "formControl" type = "text" required>
+                <input name = "surname" class = "formControl" type = "text" required>
             </div>
             <div class="form-group-2">
                 <label for = "firstname"> Firstname </label>
-                <input name = "firstname" value = <?php echo $user_c; ?> class = "formControl" type = "text" required>
+                <input name = "firstname" class = "formControl" type = "text" required>
             </div>
             <div class="form-group-2">
                 <label for = "middleName"> Middle Name </label>
@@ -49,7 +49,7 @@ if (isset ($_POST["register"])) {
             </div>
             <div class="form-group-2">
                 <label for = "emails"> Email </label>
-                <input name = "emails" value = <?php echo $user_d; ?> class = "formControl" type = "email" placeholder = "123@example.com" required>
+                <input name = "emails" placeholder = "123@gmail.com" class = "formControl" type = "email" placeholder = "123@example.com" required>
             </div>
             <div class="form-group-2">
                 <label for = "confirm email"> Confirm Email </label>
@@ -65,7 +65,7 @@ if (isset ($_POST["register"])) {
             </div>
         </form>
 
-        <span class = "echo"> <?php if (isset($error_a)) {echo "<br> $error_a";} ?> </span>
+        <span class = "echo"> <?php if (isset($error)) {echo "<br> $error";} ?> </span>
     </div>
 
 </body>
